24b631a0a19db205b49c1680b2ab2a5e3073798a
Ryan Margono <ryanmargono@Ryans-MacBook-Pro.local>
2018-04-01 22:40:47 -0400

    added more sources

--- client/components/Videos/Videos.js
+++ client/components/Videos/Videos.js
@@ -7,25 +7,27 @@ import Player from './Player'
 import { addVideo } from '../../store/currentVideo'
 import { Redirect } from 'react-router-dom'
 
-const Videos = props => {
+class Videos extends React.Component{
+    componentWillMount(){
+        Promise.all(this.props.artists.map(artist => this.props.fetchVideos(artist)))
+    }
     
-    const selectVideo = e => props.addVideo(e.target.value)
-
-    if (!props.artists.length) return <Redirect to="/artists"/>
-    if (!props.videos.length) Promise.all(props.artists.map(artist => props.fetchVideos(artist)))
-    return (
-        <div>
+    render(){
+        const selectVideo = e => this.props.addVideo(e.target.value)
+        if (!this.props.artists.length) return <Redirect to="/artists"/>
+        return(
+            <div>
             <Row> <Nav /> </Row>
             <Row style={{ paddingLeft: 10 }}> <h2> Videos </h2> </Row>
-            { props.currentVideo && <Row style={{paddingLeft:10, paddingRight:10}}><Player url={props.currentVideo} /> </Row>}
+            { this.props.currentVideo && <Row style={{paddingLeft:10, paddingRight:10}}><Player url={this.props.currentVideo} /> </Row>}
             <Row style={{ paddingLeft: 10, paddingRight: 10 }}>
                 <Collapsible>
                     {
-                        props.videos.length &&
-                        props.artists.map(artist => (
+                        this.props.videos.length &&
+                        this.props.artists.map(artist => (
                             <CollapsibleItem key={artist} header={artist}>
                                 {
-                                    props.videos.map(video => {
+                                    this.props.videos.map(video => {
                                         if (video.artist === artist) return <Row key={video.url}> <Button onClick={selectVideo} value={video.url}> {video.title} </Button> </Row>
                                     })
                                 }
@@ -35,7 +37,8 @@ const Videos = props => {
                 </Collapsible>
             </Row>
         </div>
-    )
+        )
+    }
 }
 
 const mapState = store => ({ videos: store.videos, artists: store.artists, currentVideo: store.currentVideo })
--- utils/urlify.js
+++ utils/urlify.js
@@ -28,4 +28,184 @@ const urlifyBillboard = name => {
     return `https://www.billboard.com/music/${urlified}/news`
 }
 
-module.exports = { urlifyBillboard, urlifyHnhh, urlifyPf }
+const urlifyTsis = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://thissongissick.com/search?q=${urlified}`
+}
+
+const urlifyEdms = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://www.edmsauce.com/?s=${urlified}`
+}
+
+const urlifyConsequence = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `%20${word}`
+    })
+    return `https://consequenceofsound.net/?s=${urlified}`
+}
+
+const urlifyStereoGum = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://www.stereogum.com/?s=${urlified}`
+}
+
+const urlifyTinymt = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://www.tinymixtapes.com/?search=${urlified}`
+}
+
+const urlifyDancingA = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://dancingastronaut.com/?s=${urlified}`
+}
+
+const urlify2dope = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `http://2dopeboyz.com/?s=${urlified}`
+}
+
+const urlifyRapRadar = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `http://rapradar.com/?s=${urlified}`
+}
+
+const urlifyPopJus = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://www.popjustice.com/?s=${urlified}`
+}
+
+const urlifyMusicBlog = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `http://amusicblogyea.com/?s=${urlified}`
+}
+
+const urlifyAnr = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://www.anrfactory.com/?s=${urlified}`
+}
+
+const urlifyCaesar = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `http://www.caesarlivenloud.com/search?q=${urlified}`
+}
+
+const urlifyEdmNations = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://edmnations.com/?s=${urlified}`
+}
+
+const urlifyIndietronica = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `http://indietronica.org/?s=${urlified}`
+}
+
+const urlifyKings = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `http://kingsofar.com/?s=${urlified}`
+}
+
+const urlifyLive = name => {
+    const words = name.toLowerCase().split(' ')
+    let urlified = ''
+    words.forEach((word, i) => {
+        if (i == 0) urlified += word
+        else urlified += `+${word}`
+    })
+    return `https://livemusicblog.com/?s=${urlified}`
+}
+
+module.exports = {
+    urlifyPf,
+    urlifyHnhh,
+    urlifyBillboard,
+    urlifyTsis,
+    urlifyEdms,
+    urlifyConsequence,
+    urlifyStereoGum,
+    urlifyTinymt,
+    urlifyDancingA,
+    urlify2dope,
+    urlifyRapRadar,
+    urlifyPopJus,
+    urlifyMusicBlog,
+    urlifyAnr,
+    urlifyCaesar,
+    urlifyEdmNations,
+    urlifyIndietronica,
+    urlifyKings,
+    urlifyLive
+}
