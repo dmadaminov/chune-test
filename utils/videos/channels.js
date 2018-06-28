42a22da51f77d5272466f4dbebcdf382f11b521f
dreamingblackcat <dreamingblackcat10@gmail.com>
2018-06-28 11:02:42 +0630

    Added new youtbue channels and enable all of them

--- utils/videos/channels.js
+++ utils/videos/channels.js
@@ -43,74 +43,159 @@ const channels = [
     "channelId": "UCzHcC_PfVKid0BITeTkZMjQ",
     "name": "Pigeons & Planes"
   },
-  // {
-  //   "channelId": "UCRCOCvfOkoqneyQCbNOUPwg",
-  //   "name": "The Fader"
-  // },
-  // {
-  //   "channelId": "UC-JblcinswY50lrUdSaRNEg",
-  //   "name": "Rolling Stone"
-  // },
-  // {
-  //   "channelId": "UCsVcseUYbYjldc-XgcsiEbg",
-  //   "name": "Billboard"
-  // },
-  // {
-  //   "channelId": "UChnxjIaAJCJgNm8jUTb1A9g",
-  //   "name": "All Def Music"
-  // },
-  // {
-  //   "channelId": "UCc27uVVmcn23SGDYL5uIpMQ",
-  //   "name": "BVTV Music"
-  // },
-  // {
-  //   "channelId": "UChwgB3XD2h__UI3bjOk_4Fg",
-  //   "name": "RapUpTV"
-  // },
-  // {
-  //   "channelId": "UC8l9frL61Yl5KFOl87nIm2w",
-  //   "name": "BlankTV"
-  // },
-  // {
-  //   "channelId": "UCFAKGci5lneha2x4XMbzYrQ",
-  //   "name": "La Blogothèque"
-  // },
-  // {
-  //   "channelId": "UCNNTZgxNQuBrhbO0VrG8woA",
-  //   "name": "No Jumper"
-  // },
-  // {
-  //   "channelId": "UCg7lal8IC-xPyKfgH4rdUcA",
-  //   "name": "DJ Vlad"
-  // },
-  // {
-  //   "channelId": "UC-yXuc1__OzjwpsJPlxYUCQ",
-  //   "name": "WORLDSTARHIPHOP"
-  // },
-  // {
-  //   "channelId": "UCiTFwf4VFGMyfg3cQlXP9JQ",
-  //   "name": "NME"
-  // },
-  // {
-  //   "channelId": "UCoBMK97ZfZmRCqIwMQYdicA",
-  //   "name": "iHeartRadio"
-  // },
-  // {
-  //   "channelId": "UCIhTZA7_-jz6nyxW2LePWLQ",
-  //   "name": "Virgin Radio UK"
-  // },
-  // {
-  //   "channelId": "UC-FQUIVQ-bZiefzBiQAa8Fw",
-  //   "name": "BBC Radio 1"
-  // },
-  // {
-  //   "channelId": "UCUdh03xdul9ITREyFGQp4dw",
-  //   "name": "A Music Blog, Yea?"
-  // },
-  // {
-  //   "channelId": "UC8h8NJG9gacZ5lAJJvhD0fQ",
-  //   "name": "Nardwuar Serviette"
-  // }
+  {
+    "channelId": "UCRCOCvfOkoqneyQCbNOUPwg",
+    "name": "The Fader"
+  },
+  {
+    "channelId": "UC-JblcinswY50lrUdSaRNEg",
+    "name": "Rolling Stone"
+  },
+  {
+    "channelId": "UCsVcseUYbYjldc-XgcsiEbg",
+    "name": "Billboard"
+  },
+  {
+    "channelId": "UChnxjIaAJCJgNm8jUTb1A9g",
+    "name": "All Def Music"
+  },
+  {
+    "channelId": "UCc27uVVmcn23SGDYL5uIpMQ",
+    "name": "BVTV Music"
+  },
+  {
+    "channelId": "UChwgB3XD2h__UI3bjOk_4Fg",
+    "name": "RapUpTV"
+  },
+  {
+    "channelId": "UC8l9frL61Yl5KFOl87nIm2w",
+    "name": "BlankTV"
+  },
+  {
+    "channelId": "UCg7lal8IC-xPyKfgH4rdUcA",
+    "name": "DJ Vlad"
+  },
+  {
+    "channelId": "UC-yXuc1__OzjwpsJPlxYUCQ",
+    "name": "WORLDSTARHIPHOP"
+  },
+  {
+    "channelId": "UCiTFwf4VFGMyfg3cQlXP9JQ",
+    "name": "NME"
+  },
+  {
+    "channelId": "UCoBMK97ZfZmRCqIwMQYdicA",
+    "name": "iHeartRadio"
+  },
+  {
+    "channelId": "UCIhTZA7_-jz6nyxW2LePWLQ",
+    "name": "Virgin Radio UK"
+  },
+  {
+    "channelId": "UC-FQUIVQ-bZiefzBiQAa8Fw",
+    "name": "BBC Radio 1"
+  },
+  {
+    "channelId": "UCUdh03xdul9ITREyFGQp4dw",
+    "name": "A Music Blog, Yea?"
+  },
+  {
+    "channelId": "UC8h8NJG9gacZ5lAJJvhD0fQ",
+    "name": "Nardwuar Serviette"
+  },
+  {
+    "channelId": "UCvIFYR01Rp0VX5vegE_uHKQ",
+    "name": "Big Boy TV"
+  },
+
+  {
+    "channelId": "UCzvsu2bRP8Asfv3lb-pz_aw",
+    "name": "Beats 1 Radio"
+  },
+
+  {
+    "channelId": "UCBv7HEHuVlNAELGi5XJd85Q",
+    "name": "Beats 1 on Apple Music"
+  },
+
+  {
+    "channelId": "UCGBpxWJr9FNOcFYA5GkKrMg",
+    "name": "Boiler Room"
+  },
+
+  {
+    "channelId": "UCBKIrKI8ezApiTVkEknu6xg",
+    "name": "Power 106 Los Angeles"
+  },
+
+  {
+    "channelId": "UCNNTZgxNQuBrhbO0VrG8woA",
+    "name": "No Jumper"
+  },
+
+  {
+    "channelId": "UC-9EHhfnWiG4lkQgKfxuQHw",
+    "name": "Tidal"
+  },
+
+  {
+    "channelId": "UCAJnnJPeWf45gPDuQ15-Z6w",
+    "name": "Zach Sang Show"
+  },
+
+  {
+    "channelId": "UCsEukrAd64fqA7FjwkmZ_Dw",
+    "name": "GQ"
+  },
+
+  {
+    "channelId": "UCAUO8EhsSTz02stvKDupChQ",
+    "name": "MTV News"
+  },
+
+  {
+    "channelId": "UC8MPOWNoWA-B4MS_qv9i3PQ",
+    "name": "Tim Westwood"
+  },
+
+  {
+    "channelId": "UCK7IIV6Q2junGSdYK3BmZMg",
+    "name": "Jimmy Kimmel Live"
+  },
+
+  {
+    "channelId": "UCJ0uqCI0Vqr2Rrt1HseGirg",
+    "name": "The Late Late Show with James Corden"
+  },
+
+  {
+    "channelId": "UCNe161YMUykW264kFe5PWxA",
+    "name": "Highsnobiety"
+  },
+
+  {
+    "channelId": "UCWg7ZcouKaf4TQc_too6kGQ",
+    "name": "Kiss FM UK"
+  },
+
+  {
+    "channelId": "UCE560wL2ftkMrQL40LYFUVg",
+    "name": "iamOTHER"
+  },
+
+  {
+    "channelId": "UCPD_bxCRGpmmeQcbe2kpPaA",
+    "name": "First We Feast"
+  },
+
+  {
+    "channelId": "UCftwRNsjfRo08xYE31tkiyw",
+    "name": "Wired"
+  },
+  {
+    "channelId": "UCFAKGci5lneha2x4XMbzYrQ",
+    "name": "La Blogothèque"
+  }
 ];
 
 module.exports = channels;
