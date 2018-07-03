5701c06c55ee2896ec25456aa1e694e522cad2bd
dreamingblackcat <dreamingblackcat10@gmail.com>
2018-07-03 14:56:26 +0630

    Add music brainz api to autocomplete end point and refactor code for the endpoint.

--- server/autocomplete.js
+++ server/autocomplete.js
@@ -1,49 +1,19 @@
 const router = require('express').Router()
-const axios = require('axios');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const lastFmApiKey = process.env.LAST_FM_API_KEY;
-
-const withoutWords = (name, words) => {
-  var ok = true;
-  words.forEach(word => {
-    ok = ok && name.indexOf(word) == -1;
-  })
-  return ok;
-}
+const searchArtists = require('../utils/autocomplete/searchArtists');
 
 router.post('/', (req, res, next) => {
-    const artistName = req.body.name;
-    axios.get(`http://ws.audioscrobbler.com/2.0/`, {
-        params: {
-            method: 'artist.search',
-            artist: artistName,
-            api_key: lastFmApiKey,
-            format: 'json'
-        }
-    })
-        .then(function (response) {
-            const artistNames = response.data.results.artistmatches.artist.map(artist => artist.name).filter(name => {
-              return withoutWords(name, [
-                '&', 'featuring', 'feat.', 'and', ',', "Feat.", "YouTube", " x ", " ft ", " ft.", "Ft.", "(", ")",
-              ]);
-            });
-            res.json(artistNames);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+  const artistName = req.body.name;
+  if(!artistName) {
+    res.status(500);
+    res.json("Invalid query");
+  }
+  searchArtists(artistName).then(artistNames => {
+    res.json(artistNames);
+  }).catch(err => {
+    res.status(500);
+    res.json({error: err});
+  })
+
 });
-
-const toObject = (arr) => {
-    let data = {};
-
-    arr.forEach(artist => {
-        data[artist] = null
-    });
-
-    return data;
-};
 
 module.exports = router
