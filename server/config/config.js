6551ca8036e5778a7d629b6faa733b1a3b9cd9d0
dreamingblackcat <dreamingblackcat10@gmail.com>
2018-07-11 19:52:10 +0630

    Add production db config for sequelize

--- server/config/config.js
+++ server/config/config.js
@@ -22,11 +22,6 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": "chune_production",
-    "host": "127.0.0.1",
-    "port": process.env.DB_PORT,
-    "dialect": "postgres"
+    "use_env_variable": "DATABASE_URL"
   }
 }
