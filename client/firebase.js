6c07cfc7718d9dd2fe60675f8b555480caa5a158
Kieran Derfus <kieranderfus@gmail.com>
2018-07-22 20:47:30 -0400

    ADD: Client-side env variables

--- client/firebase.js
+++ client/firebase.js
@@ -8,23 +8,7 @@ const config = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-
-    // apiKey: "AIzaSyAq9AiCjXeGy8hvQOS24LxvToSHoZLLti0",
-    // authDomain: "chune-dev.firebaseapp.com",
-    // databaseURL: "https://chune-dev.firebaseio.com/",
-    // projectId: "chune-dev",
-    // storageBucket: "chune-dev.appspot.com",
-    // messagingSenderId: "243198086936"
-
-    // apiKey: "AIzaSyDuX7wruQ7Eg2MmPo15zGe2_ZJI-lbF4gE",
-    // authDomain: "chune-5aefd.firebaseapp.com",
-    // databaseURL: "https://chune-5aefd.firebaseio.com",
-    // projectId: "chune-5aefd",
-    // storageBucket: "chune-5aefd.appspot.com",
-    // messagingSenderId: "268868502826"
 };
-
-console.log("CONFIG", config);
 
 // Initialize Firebase
 firebase.initializeApp(config);
