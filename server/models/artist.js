12cba57a6553d0cf83514a7d7f15b5d15db674df
GeekO(n)Edge <isak@geekonedge.com>
2018-08-03 17:32:09 -0400

    Added seed data for initial artists

--- server/models/artist.js
+++ server/models/artist.js
@@ -2,6 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   var Artist = sequelize.define('Artist', {
     name: DataTypes.STRING,
+    disambiguation: DataTypes.STRING,
+    isFollowed: DataTypes.BOOLEAN
   }, {});
   Artist.associate = function(models) {
     // associations can be defined here
