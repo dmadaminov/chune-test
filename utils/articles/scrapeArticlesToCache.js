d8b17e008ca08b254f30092935fe9b1eae6943c0
Isak Bosman <isak@geekonedge.com>
2018-08-10 16:59:31 -0400

    Added scrape script

--- utils/articles/scrapeArticlesToCache.js
+++ utils/articles/scrapeArticlesToCache.js
@@ -14,40 +14,52 @@ const generateSha1Key = (string) => {
     return shasum.digest('hex');
 }
 
-const scrape = (name, artistId) => {
+const scrape = (name, artistId, article_count) => {
     return Promise.all(
         [name].map(name => 
             Promise.all([
                 articleSources.fetchBillboard(name),
                 articleSources.fetchPf(name),
                 articleSources.fetchHnhh(name),
-                //articleSource.fetchTsis(name),
+                articleSource.fetchTsis(name),
                 articleSources.fetch_your_edm(name),
                 //articleSources.fetch_pigeon_planes(name),
                 articleSources.fetch_louder_sound(name),
-                //articleSources.fetch_ucr(name),
-                //articleSources.fetch_cmt(name)
+                articleSources.fetch_ucr(name),
+                articleSources.fetch_cmt(name)
                    ])))
         .then(matches => {
+            // Keep count of articles list so we can log it
+            article_count += matches.length
+            
             return Promise.all(_.flattenDeep(matches).map(article => {
+                
                 article.artistId = artistId;
                 article.lastUpdatedAt = moment().toDate();
                 article.date = article.date ? moment(article.date).toDate() : null;
                 
-                return firestore.collection('articles').doc(generateSha1Key(`${artistId}:${article.url}`)).set(article, {merge: true})
+                if (article.date) {
+                    firestore.collection('articles').doc(generateSha1Key(`${artistId}:${article.url}`)).set(article, {merge: true})
+                }
             }));
         }).then(results => {
-            return firestore.collection('artists').doc(artistId).set({ articlesLastFetchedAt: moment().toDate() }, { merge: true })     
+            firestore.collection('artists').doc(artistId).set({ articlesLastFetchedAt: moment().toDate() }, { merge: true })
+            console.log('Done processing: ', name);
         }).catch(err => {
             console.log("ERR", err);
         })
 };
 
 
-const fetchArticles = (name) => {
-    return fetchArtist(name).then(artist => {
-        return scrape(name, artist.artistId);
-    });
+const fetchArticles = (artists, count) => {
+    return Promise.all(artists.map(artist => {
+        return fetchArtist(artist.name).then(artist => {
+            return scrape(name, artist.artistId, count)
+                .catch(err => { console.log("Failed to scrape artist:", err); });
+        }).catch(err => {
+            console.log("Failed to fetch Artist: ", err)
+        });
+    }));
 }
 
 module.exports = fetchArticles;
--- utils/scripts/rescrapeArticlesForAllArtists.js
+++ utils/scripts/rescrapeArticlesForAllArtists.js
@@ -1,13 +1,4 @@
 #!/usr/bin/env node
-// This script uses firebase admin SDK to fetch all users following artists
-// and then fetching all articles for those artists.
-// This is essentially warming the cache. 
-// If we're seeing slow loading of articles in the future (when there are many users),
-// please set up a cron job that will run this script automatically on every X mins or so.
-// Depending on how often you want to warm the cache and how long did you set up the cache burst time.
-// When deciding to run this on cronb job,
-// please keep in mind that the script run time will grow with the number of artists in the system.
-// Might not be worth it to run very frequently. Servers could freeze up.
 
 const admin = require('../firebase/firebaseAdmin');
 const moment = require('moment');
@@ -20,22 +11,15 @@ const usersRef = db.ref('users');
 const startTime = moment();
 
 usersRef.once('value', (snapshot) => {
-  // console.log("Users snapshot", snapshot.val());
-  const users = snapshot.val();
-  const nameLists = Object.keys(users).map((userId) => {
-    const user = users[userId];
-    const artists = Object.keys(user.artists);
-    return artists;
-  });
-  const names = _.chain(nameLists).flattenDeep().uniq().value();
-  Promise.all(
-      names.map(name => {
-          return scrapeArtist(name);
-      })
-  ).then(results => {
-    const endTime = moment();
-    console.log("Filled all article caches. Time taken in seconds => ", endTime.diff(startTime, 'seconds'));
-  }).catch(err => {
-    console.log("Err filling all caches", err);
-  })
+    // console.log("Users snapshot", snapshot.val());
+    const users = snapshot.val();
+    const nameLists = Object.keys(users).map((userId) => {
+        const user = users[userId];
+        const artists = Object.keys(user.artists);
+        return artists;
+    });
+    const names = _.chain(nameLists).flattenDeep().uniq().value();
+    scrapeArtist(names, count)
+        .then(results => { process.exit(0) })
+        .catch(err => { console.log("", err); });
 });
