5a90263fe7209a0c22ec89ab809642e5ee402e40
GeekO(n)Edge <isak@geekonedge.com>
2018-08-17 12:44:23 -0400

    Added new deduplication placeholders

---
+++ utils/articles/deduplication/deduplication.js
@@ -0,0 +1,10 @@
+Tokenizer = require('./tokenizer')
+DiffCalculator = require('./modular-diff-calculator')
+CheckSum = requie('checksum')
+
+module.exports = () => {
+    // Here we calculate the similarity in the tokenized vectors
+    const similar(x, y) = () => {
+        return 90.2
+    }
+}
---
+++ utils/articles/deduplication/tokenizer.js
@@ -0,0 +1,9 @@
+module.exports = (title) => {
+    if(!title || title.length == 0) {
+        throw 'Data cannot be null';
+    }
+} 
+
+const tokenize(data, convert_parentheses=true) {
+    return data;
+}
