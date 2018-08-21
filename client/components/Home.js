3396938d7e7421151072c35ef38c3d0ac57c8547
Tomislav Horaček <tomislav.horacek@gmail.com>
2018-08-21 22:27:41 +0200

    [th-feature-2] Introduce main article component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,165 +1,29 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import Artists from './Artists/Artists'
-import Navbar from './Navbar'
-import { Row, Collapsible, CollapsibleItem, Modal, Button, ProgressBar, Col, Card, CardTitle } from 'react-materialize'
-import { fetchRecentEntriesForMultipleArtists, clearRecentEntries } from '../store/recentEntries'
-import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 
-import { Redirect } from 'react-router-dom'
-import { database, auth } from '../firebase'
-import { addUser } from '../store/user';
-import { fetchArticles } from '../store/articles'
-import { timestampToDate } from '../helpers/populateArticles'
-import { addArtists } from '../store/artists'
-import VideoPlayer from './Videos/Player'
-import '../assets/global.css'
-import '../assets/landing.css'
-import ArticleCard from './News/Article'
-import VideoCard from './Videos/Video'
-import SearchForm from './SearchForm'
-import Waypoint from 'react-waypoint';
-import Loading from './shared/Loading';
-import EmptyList from './shared/EmptyList';
-import { withRouter } from 'react-router-dom'
+import Navbar from './Navbar'
+import Paper from '@material-ui/core/Paper';
 
-const styles = theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: "#fafafa",
-  },
-  gridList: {
-    width: 716,
-    borderRadius: 4,
-  },
-  subheader: {
-    width: '100%',
-  },
-  gridRow: {
-    height: "auto",
-    marginBottom: 24,
-    width: '100%'
-  },
-  container: {
-    backgroundColor: "#fafafa",
-    width: '100%',
-    paddingTop: 24,
-  },
-  noentries: {
-    width: 716,
-    height: 300,
-    margin: '178px auto',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
+import BasicArticleCard from './blocks/BasicArticleCard/BasicArticleCard';
 
-class Home extends React.Component {
+import mainStyles from './Home.css';
 
-  componentDidMount() {
-    //fetch latest recent entries list
-    if (this.props.artists.length >= 0) {
-      this.props.fetchRecentEntriesForMultipleArtists(this.props.artists.map(artist => artist.name))
-    }
-  }
-
-  componentDidMount() {
-    this.props.clearRecentEntries();
-    this.props.fetchRecentEntriesForMultipleArtists(this.props.artists.map(artist => artist.name))
-  }
-
-  _renderWaypoint = () => {
-    if (!this.props.fetching && !this.props.endOfList) {
-      return (
-        <Waypoint onEnter={this._loadMoreItems} threshold={2.0} />
-      );
-    } else {
-      return this.props.endOfList ? null : <Loading />;
-    }
-  }
-
-  _renderItems = (recentEntries) => {
-    const {classes} = this.props;
-
-    return recentEntries.map(item => {
-      return (
-        <li className={classes.gridRow} key={`${item.url}::${item.ID}`} >
-          {
-            item.isVideo
-            ? <VideoCard video={item} autoplay={false}/>
-            : <ArticleCard article={item} />
-          }
-        </li>
-      )
-    })
-  }
-
-  _loadMoreItems = () => {
-    const props = this.props;
-    props.fetchRecentEntriesForMultipleArtists(props.artists.map(artist => artist.name), props.currentPage + 1);
-  }
-
+export default class Home extends React.Component {
   render() {
-    const { classes, artists, recentEntries, initialLoading } = this.props;
 
-    if (!artists.length) return <Redirect to="/artists"/>
+    return (
+      <div>
+        <Navbar value={0} />
 
-    if(!initialLoading) {
-      if (recentEntries.length) {
-        return (
-          <div>
-            <Navbar value={0} />
-            <Paper className={classes.container}>
-              <div className={classes.root}>
-                <ul className={classes.gridList}>
-                  {this._renderItems(recentEntries)}
-                  {this._renderWaypoint()}
-                </ul>
-              </div>
-            </Paper>
-          </div>
-        );
-      } else {
-
-        return (
-          <div>
-            <Navbar value={0} />
-            <EmptyList 
-              messageOne={"Sorry, no recent media about your artists."}
-              messageTwo={"Try using the search bar to follow another artist. Or go to artists page to follow artists related to your favorite ones."} />
-          </div>
-        )
-      }
-    } else {
-      return (
-        <div>
-          <Navbar value={0} />
-          <Loading />
+        <div className='homePageWrapper'>
+          <BasicArticleCard
+            image='https://www.billboard.com/files/styles/article_main_image/public/media/shakira-june-2018-billboard-1548.jpg'
+            title='Smino Brings Out T-Pain For Epic "Chopped N Skrewed" Performance In Atlanta'
+            resourceName='via hotnewhiphop'
+          />
         </div>
-      )
-    }
+
+      </div>
+    )
   }
 }
-
-const mapDispatch = dispatch => ({ 
-  fetchRecentEntriesForMultipleArtists: (names, page) => dispatch(fetchRecentEntriesForMultipleArtists(names, page)),
-  clearRecentEntries: () => dispatch(clearRecentEntries()),
-})
-const mapState = store => ({ 
-  recentEntries: store.recentEntries.recentEntries,
-  currentPage: store.recentEntries.currentPage,
-  fetching: store.recentEntries.fetching,
-  endOfList: store.recentEntries.endOfList,
-  initialLoading: store.recentEntries.initialLoading,
-  artists: store.followingArtists.artists,
-  userID: store.user.uid,
-})
-
-export default withStyles(styles)(withRouter(connect(mapState, mapDispatch)(Home)));
-
-
---
+++ client/components/blocks/BasicArticleCard/BasicArticleCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import mainStyles from './BasicArticleCard.css';
+
+
+export default class BasicArticleCard extends React.Component {
+  render() {
+
+    const { image, title, resourceName } = this.props;
+
+    return (
+      <div
+        className='basicArticleCardWrapper'
+        style={{ backgroundImage: `url(${image ? image : null})` }}
+      >
+        <h2 className='title'>{title}</h2>
+        <p className='resourceName'>{resourceName}</p>
+      </div>
+    )
+  }
+}
