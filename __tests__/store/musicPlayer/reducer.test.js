686d11f258bf8db2f30c024b9d90f10e5f64802d
Studenov Roman <rstudenov@allmax.team>
2018-09-07 11:51:12 +0700

    finish music player reducer tests

--- __tests__/store/musicPlayer/reducer.test.js
+++ __tests__/store/musicPlayer/reducer.test.js
@@ -11,9 +11,28 @@ describe('Music player reducer', () => {
 
   it('should get playlist', () => {
     const playlist = [{ id: 219, title: 'The Greatest' }, { id: 398, title: 'From the heart' }];
-    const action = { type: TYPES.GET_MUSIC_PLAYLIST, payload: { playlist }};
+    const action = { type: TYPES.GET_MUSIC_PLAYLIST, payload: { playlist } };
     const nextState = reducer(initState, action);
 
     expect(nextState).toEqual({ ...initState, playlist });
+  });
+
+  it('should play music track', () => {
+    const track = 1;
+    const action = { type: TYPES.PLAY_MUSIC_PLAYER, payload: { track } };
+    const nextState = reducer(initState, action);
+
+    expect(nextState).toEqual({
+      ...initState, playMusic: true, track,
+      modal: true
+    });
+  });
+
+  it('should pause music player', () => {
+    const currentTime = '3.14';
+    const action = { type: TYPES.PAUSE_MUSIC_PLAYER, payload: { currentTime } };
+    const nextState = reducer(initState, action);
+
+    expect(nextState).toEqual({ ...initState, currentTime, playMusic: false });
   });
 });
