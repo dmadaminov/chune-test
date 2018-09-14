b9a52f2ee3c51862eee3634009b2d36af0990a3a
Studenov Roman <rstudenov@allmax.team>
2018-09-14 17:55:13 +0700

    +

--- src/store/artists/utilities/artistsUser.js
+++ src/store/artists/utilities/artistsUser.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getList = (token) => {
-  const url = 'https://chune-api.herokuapp.com/api/v1/artists';
+  const url = 'https://chune-api.herokuapp.com/api/v1/artists/';
   return axios.get(url, {
     headers: {
       Authorization: `JWT ${token}`
