7f8e50709af7b3d4b8824d10ab37eab6196170f8
Studenov Roman <rstudenov@allmax.team>
2018-09-18 13:36:05 +0700

    +

--- src/store/content/sagas.js
+++ src/store/content/sagas.js
@@ -1,14 +1,17 @@
 import { put, takeEvery, call } from 'redux-saga/effects';
 
-import { SUCCESS_GET_TOKEN } from '../auth/types';
+import { SUCCESS_GET_USER_ARTISTS } from '../artists/types';
 import { errorMessage } from '../error/actions';
 import { successGetContentHomepage } from './actions';
 import { getContentToServer } from './utilities/content';
 
-export function* getContent() {
+export function* getContent({ payload }) {
+  const { artists } = payload;
+  let follow = false;
+  if (artists.length > 0) follow = true;
   try {
-    const data = yield call(getContentToServer);
-    const artistTracks = data.artist_tracks;
+    const data = yield call(getContentToServer, follow);
+    const artistTracks = data.artist_tracks || [];
     const contentFeed = data.content_feed;
     yield put(successGetContentHomepage(artistTracks, contentFeed));
   } catch (e) {
@@ -17,5 +20,5 @@ export function* getContent() {
 }
 
 export function* sagasContent() {
-  yield takeEvery(SUCCESS_GET_TOKEN, getContent);
+  yield takeEvery(SUCCESS_GET_USER_ARTISTS, getContent);
 }
--- src/store/content/utilities/content.js
+++ src/store/content/utilities/content.js
@@ -1,3 +1,6 @@
 import { API } from '../../../utilities/APIConfig';
 
-export const getContentToServer = () => API.get('content/').then(response => response.data);
+export const getContentToServer = (follow) => {
+  if (follow) return API.get('content/').then(response => response.data);
+  return API.get('content/?filter=recent').then(response => response.data);
+};
