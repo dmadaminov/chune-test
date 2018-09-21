d3331e4a67c3d39e9401564113d81373afa354cd
Studenov Roman <rstudenov@allmax.team>
2018-09-21 17:11:56 +0700

    fixed test artists

--- __tests__/store/artists/reducer.test.js
+++ __tests__/store/artists/reducer.test.js
@@ -4,9 +4,10 @@ import * as TYPES from '../../../src/store/artists/types';
 describe('Artists reducer', () => {
   it('should success get artists list', () => {
     const artists = [{ name: 'Drake', genre: 'POP' }, { name: 'Shakira', genre: 'POP' }];
-    const action = { type: TYPES.SUCCESS_GET_USER_ARTISTS, payload: { artists } };
+    const recommended = [{ name: 'Drake', genre: 'POP' }, { name: 'Shakira', genre: 'POP' }];
+    const action = { type: TYPES.SUCCESS_GET_USER_ARTISTS, payload: { artists, recommended } };
     const nextState = reducer(initState, action);
 
-    expect(nextState).toEqual({ ...initState, artists });
+    expect(nextState).toEqual({ ...initState, artists, recommended });
   });
 });
